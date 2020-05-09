7a777fc02bc37ac495511b86848f24f7aa8a9171
mtcarpenter <dreamlixc@163.com>
2020-05-09 23:13:52 +0800

    gateway

---
+++ mall-gateway/src/main/java/com/mtcarpenter/mall/MallGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.mtcarpenter.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mtcarpenter
+ * @github https://github.com/mtcarpenter/spring-cloud-learning
+ * @desc 微信公众号：山间木匠
+ */
+
+@SpringBootApplication
+public class MallGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+}
