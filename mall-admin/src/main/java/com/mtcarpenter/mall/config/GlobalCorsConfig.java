a1ede97e178d473b1af713073275a36e89f2e402
mtcarpenter <dreamlixc@163.com>
2020-05-10 18:49:56 +0800

    gateway cros

--- mall-admin/src/main/java/com/mtcarpenter/mall/config/GlobalCorsConfig.java
+++ mall-admin/src/main/java/com/mtcarpenter/mall/config/GlobalCorsConfig.java
@@ -10,13 +10,13 @@ import org.springframework.web.filter.CorsFilter;
  * 全局跨域配置
  * Created by macro on 2019/7/27.
  */
-//@Configuration
+@Configuration
 public class GlobalCorsConfig {
 
     /**
      * 允许跨域调用的过滤器
      */
-//    @Bean
+    @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         //允许所有域名进行跨域调用
