16f02a6ed6e61902371535d7e31f03f1d19d7eb4
mtcarpenter <dreamlixc@163.com>
2020-06-07 23:29:14 +0800

    整合 zipkin 和 sleuth

--- mall-storage/src/main/java/com/mtcarpenter/mall/StorageApplication.java
+++ mall-storage/src/main/java/com/mtcarpenter/mall/StorageApplication.java
@@ -2,13 +2,14 @@ package com.mtcarpenter.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author mtcarpenter
  * @github https://github.com/mtcarpenter/mall-cloud-alibaba
  * @desc 微信公众号：山间木匠
  */
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class StorageApplication {
     public static void main(String[] args) {
         SpringApplication.run(StorageApplication.class, args);
